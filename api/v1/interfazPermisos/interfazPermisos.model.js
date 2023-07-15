6e4dc4b864c588fc9b1a0da1eec866978165f89b
Luijhy Guerra <qdsjguerra@utp.edu.pe>
2023-07-15 02:57:39 -0500

    fix apis and config sequelize

--- api/v1/interfazPermisos/interfazPermisos.model.js
+++ api/v1/interfazPermisos/interfazPermisos.model.js
@@ -1,94 +1,79 @@
-const BaseModel = require("../../libs/baseModel");
 const queryBuilder = require("../../libs/queryBuilder");
+
 const DB = {};
-DB.obtenerDatos = ({ id_cargos }) => {
-  return new Promise((resolve, reject) => {
-    var cargos = [];
-    var cargosColumnas = [];
-    if (id_cargos != "") {
-      cargos = id_cargos.split(",");
-    }
-    for (let index = 0; index < cargos.length; index++) {
-      const cargo = cargos[index];
-      cargosColumnas.push(
-        `sum(IF(cargos_id_cargo = ${cargo},activo,0)) cargo_${cargo}`
-      );
-    }
-    var query = new queryBuilder()
-      .select(
-        [
-          [" interfaz_principales.nombre ", "interfaz_nombre"],
-          "interfaz_secundarias.id",
-          ["interfaz_secundarias.nombre ", "funcionalidad_nombre"],
-          "interfaz_secundarias.nombre_clave",
-        ].concat(cargosColumnas)
-      )
+
+DB.obtenerDatos = async ({ id_cargos }) => {
+  try {
+    const cargos = id_cargos !== "" ? id_cargos.split(",") : [];
+    const cargosColumnas = cargos.map((cargo) => {
+      return `sum(IF(cargos_id_cargo = ${cargo}, activo, 0)) cargo_${cargo}`;
+    });
+
+    const query = new queryBuilder()
+      .select([
+        ["interfaz_principales.nombre", "interfaz_nombre"],
+        "interfaz_secundarias.id",
+        ["interfaz_secundarias.nombre", "funcionalidad_nombre"],
+        "interfaz_secundarias.nombre_clave",
+        ...cargosColumnas
+      ])
       .from("interfaz_principales")
-      .leftJoin(
-        `interfaz_secundarias ON interfaz_secundarias.interfaz_principales_id = interfaz_principales.id
-        LEFT JOIN
-    interfaz_permisocargo ON interfaz_permisocargo.interfaz_secundarias_id = interfaz_secundarias.id`
-      )
+      .leftJoin(`
+        interfaz_secundarias ON interfaz_secundarias.interfaz_principales_id = interfaz_principales.id
+        LEFT JOIN interfaz_permisocargo ON interfaz_permisocargo.interfaz_secundarias_id = interfaz_secundarias.id
+      `)
       .groupBy("interfaz_secundarias.id")
       .orderBy("interfaz_principales.id,interfaz_secundarias.id")
       .toString();
-    // resolve(query);
-    // return;
-    pool.query(query, (error, res) => {
-      if (error) {
-        console.log(error);
-        reject(error);
-      }
-      resolve(res);
-    });
-  });
+
+    const res = await pool.query(query);
+    return res;
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
 };
-DB.actualizarDatos = (data) => {
-  return new Promise((resolve, reject) => {
-    var query = new queryBuilder("interfaz_permisocargo")
+
+DB.actualizarDatos = async (data) => {
+  try {
+    const query = new queryBuilder("interfaz_permisocargo")
       .insert(data)
       .merge()
       .toString();
-    // resolve(query);
-    // return;
-    pool.query(query, (error, res) => {
-      if (error) {
-        console.log(error);
-        reject(error);
-      }
-      resolve(res);
-    });
-  });
+
+    const res = await pool.query(query);
+    return res;
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
 };
-DB.obtenerPermisoInterfaz = ({ id_ficha, id_acceso, nombres_clave }) => {
-  return new Promise((resolve, reject) => {
-    var nombres_claveList = nombres_clave.split(",");
-    var nombres_claveProcesada = [];
-    for (let index = 0; index < nombres_claveList.length; index++) {
-      const nombre_clave = nombres_claveList[index].trim();
-      nombres_claveProcesada.push(
-        `sum(if(nombre_clave='${nombre_clave}',activo,0) ) ${nombre_clave}`
-      );
-    }
-    var query = new queryBuilder("fichas_has_accesos")
+
+DB.obtenerPermisoInterfaz = async ({ id_ficha, id_acceso, nombres_clave }) => {
+  try {
+    const nombres_claveList = nombres_clave.split(",");
+    const nombres_claveProcesada = nombres_claveList.map((nombre_clave) => {
+      return `sum(if(nombre_clave='${nombre_clave.trim()}', activo, 0)) ${nombre_clave.trim()}`;
+    });
+
+    const query = new queryBuilder("fichas_has_accesos")
       .select(nombres_claveProcesada)
-      .leftJoin(
-        `interfaz_permisocargo ON interfaz_permisocargo.cargos_id_Cargo = fichas_has_accesos.cargos_id_Cargo
-            inner JOIN
-        interfaz_secundarias ON interfaz_secundarias.id = interfaz_secundarias_id`
-      )
+      .leftJoin(`
+        interfaz_permisocargo ON interfaz_permisocargo.cargos_id_Cargo = fichas_has_accesos.cargos_id_Cargo
+        INNER JOIN interfaz_secundarias ON interfaz_secundarias.id = interfaz_secundarias_id
+      `)
       .where([
         `Fichas_id_ficha = ${id_ficha}`,
-        `Accesos_id_acceso = ${id_acceso}`,
+        `Accesos_id_acceso = ${id_acceso}`
       ])
       .toString();
-    pool.query(query, (error, res) => {
-      if (error) {
-        console.log(error);
-        reject(error);
-      }
-      resolve(res ? res[0] : {});
-    });
-  });
+
+    const res = await pool.query(query);
+    return res ? res[0] : {};
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
 };
+
 module.exports = DB;
